273d349a2513982c3f4385516c21e8a846d73b1a
jayasurya17 <jayasurya1796@gmail.com>
2022-10-04 20:31:21 -0500

    Admin mobile friendly navbar

--- V2frontend/src/Pages/Admin/Home.jsx
+++ V2frontend/src/Pages/Admin/Home.jsx
@@ -1,17 +1,35 @@
 
-import { AppShell, Header, Title } from '@mantine/core';
+import { AppShell, Header, Title, MediaQuery, Burger, useMantineTheme } from '@mantine/core';
 import { Outlet } from 'react-router-dom'
 import Navbar from "./Navbar"
+import { useState } from 'react';
 
 function Home() {
+  const [opened, setOpened] = useState(false);
+  const theme = useMantineTheme();
+
   return (
     <AppShell
       padding="md"
-      navbar={<Navbar ></Navbar>
+      navbar={
+        <Navbar opened={opened} setOpened={setOpened}></Navbar>
       }
-      header={<Header height={60} p="md" >
-        <Title order={4}>DeclareGame.in Admin</Title>
-      </Header>}
+      header={
+        <Header height={60} p="md" >
+          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+              <Burger
+                opened={opened}
+                onClick={() => setOpened((o) => !o)}
+                size="sm"
+                color={theme.colors.gray[6]}
+                mr="xl"
+              />
+            </MediaQuery>
+            <Title order={4}>DeclareGame.in Admin</Title>
+          </div>
+        </Header>
+      }
       styles={(theme) => ({
         main: { backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] },
       })}
